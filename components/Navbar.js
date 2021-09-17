4ed9ec8340e14294d79d628ba7ba763a8ddf1b85
Zoheb Boga <zohebboga@hotmail.com>
2021-09-17 17:31:23 -0400

    Added Smooth scroll to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,24 @@
 import styles from '../styles/Navbar.module.css'
 
 const Navbar = () => {
+  const handleClick = (e) => {
+    e.preventDefault()
+    const link = e.target.getAttribute('href')
+    const offset = document.querySelector(link).offsetTop
+    
+    window.scrollTo({
+      top: offset - 64
+    })
+  }
   return (
     <div className={styles.navContainer}>
       <h1>Nav Logo</h1>
       <ul className={styles.navLinks}>
-        <li> <a href='#home'>Home</a></li>
-        <li> <a href='#projects'> Projects</a></li>
-        <li> <a href='#articles'> Articles</a></li>
-        <li> <a href='#resume'> Resume</a></li>
-        <li> <a href='#contact'> Contact</a></li>
+        <li> <a onClick={handleClick} href='#home'>Home</a></li>
+        <li> <a onClick={handleClick} href='#projects'> Projects</a></li>
+        <li> <a onClick={handleClick} href='#articles'> Articles</a></li>
+        <li> <a onClick={handleClick} href='#resume'> Resume</a></li>
+        <li> <a onClick={handleClick} href='#contact'> Contact</a></li>
       </ul>
       <div>
         Light
