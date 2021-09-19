5ede8c16d30274c9547af2240847248a1f9e7226
Zoheb Boga <zohebboga@hotmail.com>
2021-09-19 18:07:56 -0400

    Minor style changes to home

--- components/Home.js
+++ components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     <div className={styles.homeContainer}>
       <div className={styles.homeLeft}>
         <h1>Hey! I'm Zoheb <span>✌️</span></h1>
-        <h2>Full-Stack Web Developer</h2>
+        <h4>Full Stack Web Developer</h4>
       </div>
       <div className={styles.homeRight}>
         <img src={'profile.png'} />
